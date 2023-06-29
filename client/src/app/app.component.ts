a627609f15a30a3bb32c04f33370e0f421362c11
knowhatesle <kenan.kajtazovic.01@gmail.com>
2023-06-29 20:07:35 +0200

    Add nav bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7240/api/users').subscribe({
+    this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
